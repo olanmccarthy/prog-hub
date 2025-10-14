d743e86d62c439cc58b5a46c35f2ce825ca7b1f0
Olan McCarthy <olan.mccarthy@vertigenius.com>
2025-10-14 13:07:53 +0100

    add husky, add api route for typeorm check

---
+++ lib/data-source.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from '@/entities/User';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'db',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: true,
+  entities: [User],
+});
