4f1f963d3c6a17255a6896e61dbcf41533fe7f34
Olan McCarthy <olan.mccarthy@vertigenius.com>
2025-10-14 13:09:14 +0100

    testing husk auto fix

--- lib/data-source.ts
+++ lib/data-source.ts
@@ -5,8 +5,7 @@ import { User } from '@/entities/User';
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST || 'db',
-  port: 3306,
-  username: process.env.MYSQL_USER,
+  port: 3306,  username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   synchronize: true,
